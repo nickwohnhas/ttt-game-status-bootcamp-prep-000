9751a68039a9b083acfdd241a6e2060fd3ea1142
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-09 23:35:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,7 +13,7 @@ WIN_COMBINATIONS.each do |win_combination|
   position_1 = board[win_index_1]
   postiion_2 = board[win_index_2]
   position_3 = board[win_index_3]
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" || postion_1 == "O" && position_2 == "O" && position_3 == "O"
-
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return 
 
 end
