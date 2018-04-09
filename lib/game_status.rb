c333769a851eb357e5583d25b5087f45285f3b3a
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-09 23:48:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ WIN_COMBINATIONS.each do |win_combination|
      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     else 
-     return false 
+      false 
     end 
   end
 end
